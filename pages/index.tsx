179fdf4d351e900b1c049b592730d9eaedc3079d
zendqgoo <zendqgoo@outlook.com>
2022-12-15 10:38:45 +0800

    First commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ const Home: NextPage = () => {
           Explore what you can do with web3sdkio&rsquo;s brand new{" "}
           <b>
             <a
-              href="https://portal.web3sdk.io/solana"
+              href="https://docs.web3sdk.io/solana"
               target="_blank"
               rel="noopener noreferrer"
               className={styles.lightPurple}
